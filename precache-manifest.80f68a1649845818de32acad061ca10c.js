c87c292fc1744b40d56f719458fb7bd0f9e33f6c
Huu Pham <phamhuu2701@gmail.com>
2020-07-11 10:54:36 +0700

    Updates

---
+++ precache-manifest.80f68a1649845818de32acad061ca10c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd4e4355973b01c3a7daa7fe4825f9f",
+    "url": "/huupham-profile/index.html"
+  },
+  {
+    "revision": "3f557a6a666bce64ca8e",
+    "url": "/huupham-profile/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "fa08c82d25c7b16e5296",
+    "url": "/huupham-profile/static/js/2.474e4e46.chunk.js"
+  },
+  {
+    "revision": "3f557a6a666bce64ca8e",
+    "url": "/huupham-profile/static/js/main.5f751737.chunk.js"
+  },
+  {
+    "revision": "0e3c65d88be307bce0ca",
+    "url": "/huupham-profile/static/js/runtime-main.a5be3db5.js"
+  }
+]);
